b3a1876188b9e2779ece5ea5b0090bcefacea2d8
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-07-09 20:36:06 +0530

    Add frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/*
+ 1. FILE PURPOSE: Handles logic for Next.
+ 2. RESPONSIBILITIES:
+    - Manage next logic.
+Integrate with related modules.
+ 3. PLANNED CONTENTS: Functions/Classes to implement Next features.
+ 4. INPUTS / OUTPUTS: Inputs and outputs for Next.
+ 5. DEPENDS ON / USED BY: Dependencies for Next module.
+*/
